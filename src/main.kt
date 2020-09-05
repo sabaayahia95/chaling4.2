084fe3da9bf42c84ddccef30b1f0baf674350142
sabaa yahia <sabaa yahia4@gmail.com>
2020-09-05 12:48:59 +0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,5 @@
+fun main(){
+    var list = listOf("saba", "samah","lool","fatimah","hanaa");
+    println("the first element called"+"   " +list.first() );
+    println("the last element called" +"   " +list.last());
+}
